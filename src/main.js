4461f008e1497a48ca34cf9580a578747d155f6b
RaulRiveraFP <120664361+RaulRiveraFP@users.noreply.github.com>
2024-01-16 19:10:14 +0000

    Enrutador

--- src/main.js
+++ src/main.js
@@ -2,21 +2,13 @@ import { header } from './componentes/header'
 import { footer } from './componentes/footer'
 import 'bootstrap'
 import './scss/styles.scss'
-// Importamos la vista por defecto (que será home)
-async function cargarVista () {
-  const componente = await import('./vistas/proyectoNuevoVista')
-  const vista = componente.default
-  // Inyectamos la vista home
-  document.querySelector('main').innerHTML = vista.template
-  // Ejecutamos la lógica de la vista
-  vista.script()
-}
-cargarVista()
 
-// Inyectamos el componente header
+import { enrutador } from './componentes/enrutador'
+
 document.querySelector('header').innerHTML = header.template
-
-// Inyectamos el componente footer
+// header.script()
 document.querySelector('footer').innerHTML = footer.template
 
-const componente = await import('./vistas/loginVista')
+enrutador.observadorRutas()
+// Cargamos la página home
+window.location = '#/home'
